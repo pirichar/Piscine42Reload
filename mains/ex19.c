375eaecf71be5afcf2ec50b24c721f6466e98989
Pier-Luc Richard <84756667+pirichar@users.noreply.github.com>
2024-04-23 10:22:45 -0400

    now testing ex18 and 19

--- mains/ex19.c
+++ mains/ex19.c
@@ -1,41 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ex19.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pirichar <pirichar@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/19 14:32:06 by pirichar          #+#    #+#             */
-/*   Updated: 2024/04/23 10:16:53 by pirichar         ###   ########.fr       */
+/*   Created: 2024/04/23 10:16:38 by pirichar          #+#    #+#             */
+/*   Updated: 2024/04/23 10:19:46 by pirichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./ex12_ex13_.c"
-#include "./ex14.c"
-#include "./ex15.c"
-#include "./ex16.c"
-#include "./ex17.c"
-#include "./ex18.c"
-#include "./ex19.c"
-#include "./ex20.c"
-#include "./ex21.c"
-#include "./ex22.c"
-#include "./ex23.c"
-#include "./ex25.c"
+#include <stdlib.h>
+#include <stdio.h>
 
 
-int main(void)
+void ex19(void)
 {
-	ex12_ex13();
-	ex14();
-	ex15();
-	ex16();
-	ex17();
-	ex18();
-	ex19();
-	ex20();
-	ex21();
-	ex22();
-	ex23();
-	ex25();
+	printf("EX19 - Will SORT out the 3 params test3 test2 test1\n");
+	fflush(stdout);  // Flush the buffer to ensure all printf content is output before write is called
+	system("./sort_params test3 test2 test1");
 }
--- mains/test.sh
+++ mains/test.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
 gcc -Wall -Werror -Wextra main.c
+gcc -Wall -Werror -Wextra ../ex18/ft_print_params.c -o print_params
+gcc -Wall -Werror -Wextra ../ex19/ft_sort_params.c -o sort_params
 ./a.out
 rm a.out
+rm print_params
+rm sort_params
